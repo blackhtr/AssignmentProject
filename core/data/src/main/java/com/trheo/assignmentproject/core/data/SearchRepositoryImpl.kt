e815c83374d295ff61d30bee2f2359a0acbbfc91
heotaeryang <blackhtr@gmail.com>
2024-12-10 03:50:58 +0900

    exception 예외처리 추가

--- core/data/src/main/java/com/trheo/assignmentproject/core/data/SearchRepositoryImpl.kt
+++ core/data/src/main/java/com/trheo/assignmentproject/core/data/SearchRepositoryImpl.kt
@@ -3,7 +3,6 @@ package com.trheo.assignmentproject.core.data
 import com.trheo.assignmentproject.core.data.service.PixabayApi
 import com.trheo.assignmentproject.core.data.service.WikiApi
 import com.trheo.assignmentproject.core.domain.entity.ImageResultInfo
-import com.trheo.assignmentproject.core.domain.entity.SearchResultInfo
 import com.trheo.assignmentproject.core.domain.entity.WikiResultInfo
 import com.trheo.core.domain.repository.SearchRepository
 
@@ -13,30 +12,38 @@ class SearchRepositoryImpl(
 ) : SearchRepository {
     // 위키 검색
     override suspend fun searchWiki(query: String): List<WikiResultInfo> {
-        val response = wikiApi.getRelatedPages(query)
-        return response.pages.map { page ->
-            WikiResultInfo(
-                title = page.title,
-                keyword = page.titles.canonical,
-                description = page.extract,
-                imageUrl = page.originalimage?.source,
-                webViewUrl = page.content_urls.mobile.page
-            )
+        return try {
+            val response = wikiApi.getRelatedPages(query)
+            response.pages.map { page ->
+                WikiResultInfo(
+                    title = page.title,
+                    keyword = page.titles.canonical,
+                    description = page.extract,
+                    imageUrl = page.originalimage?.source,
+                    webViewUrl = page.content_urls.mobile.page
+                )
+            }
+        } catch (e: Exception) {
+            emptyList()
         }
     }
     // 이미지 검색
     override suspend fun searchImages(query: String, page: Int): List<ImageResultInfo> {
-        val response = pixabayApi.getImages(query = query, page = page)
-        return response.hits.map { hit ->
-            ImageResultInfo(
-                user = hit.user,
-                tags = hit.tags,
-                width = hit.imageWidth,
-                height = hit.imageHeight,
-                views = hit.views,
-                downloads = hit.downloads,
-                imageUrl = hit.largeImageURL
-            )
+        return try {
+            val response = pixabayApi.getImages(query = query, page = page)
+            response.hits.map { hit ->
+                ImageResultInfo(
+                    user = hit.user,
+                    tags = hit.tags,
+                    width = hit.imageWidth,
+                    height = hit.imageHeight,
+                    views = hit.views,
+                    downloads = hit.downloads,
+                    imageUrl = hit.largeImageURL
+                )
+            }
+        } catch (e: Exception) {
+            emptyList()
         }
     }
 }
